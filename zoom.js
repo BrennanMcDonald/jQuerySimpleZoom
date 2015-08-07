649a7fdceeebc13738baa4ef0b8dd7afcd1455be
Brennan McDonald <brennan.mcdonald.mail@gmail.com>
2015-08-06 23:18:49 -0400

    Removed need for huge list

--- zoom.js
+++ zoom.js
@@ -16,32 +16,6 @@
         7 : 55,
         8 : 56,
         9 : 57,
-        A : 65,
-        B : 66,
-        C : 67,
-        D : 68,
-        E : 69,
-        F : 70,
-        G : 71,
-        H : 72,
-        I : 73,
-        J : 74,
-        K : 75,
-        L : 76,
-        M : 77,
-        N : 78,
-        O : 79,
-        P : 80,
-        Q : 81,
-        R : 82,
-        S : 83,
-        T : 84,
-        U : 85,
-        V : 86,
-        W : 87,
-        X : 88,
-        Y : 89,
-        Z : 90,
         UP : 38,
         DOWN: 40,
         LEFT : 37,
@@ -68,17 +42,22 @@
             if (options.in !== null){
                 if (typeof(options.in) === "number"){
                     options.in = options.in;
-                } else if (typeof(options.in) === "string"){
-                    options.in = KEYS[options.in.toUpperCase()];
-                }
+                } else if (typeof(options.in) === "string" && options.in.length < 2){
+                    options.in = options.in.toUpperCase().charCodeAt(0);
+                } else {
+					options.in = KEYS[options.in.toUpperCase()];
+
+				}
 
             }
             if (options.out !== null){
                 if (typeof(options.out) === "number"){
                     options.out = options.out;
-                } else if (typeof(options.out) === "string"){
-                    options.out = KEYS[options.out.toUpperCase()];
-                }
+                } else if (typeof(options.out) === "string" && options.in.length < 2){
+                    options.out = options.out.toUpperCase().charCodeAt(0);
+                } else {
+					options.out = KEYS[options.out.toUpperCase()];
+				}
 
             }
         }
